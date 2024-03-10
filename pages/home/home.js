db84a6e55cbfb5c51dcacf97128436a84acb2ac0
lxh0113 <3358654275@qq.com>
2024-03-10 21:42:53 +0800

    关系绑定和AI刷题以及通知界面的完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,6 +8,73 @@ Page({
 
   },
 
+  toRelationship:function toRelationship(){
+    wx.navigateTo({
+      url: '/pages/relationship/relationship',
+      success:function(res){
+      },
+      fail:function(error){
+      }
+    })
+  },
+  toNotice:function toNotice(){
+    wx.navigateTo({
+      url: '/pages/notice/notice',
+      success:function(res){
+      },
+      fail:function(error){
+      }
+    })
+  },
+  toAI:function toAI(){
+    wx.navigateTo({
+      url: '/pages/ai/ai',
+      success:function(res){
+      },
+      fail:function(error){
+      }
+    })
+  },
+  toPaper:function toPaper(){
+    wx.switchTab({
+      url: '/pages/paper/paper',
+    })
+  },
+  toAnalysis:function toAnalysis(){
+    wx.navigateTo({
+      url: '/pages/analysis/analysis',
+      success:function(res){
+      },
+      fail:function(error){
+      }
+    })
+  },
+  toHistory:function toHistory(){
+    wx.navigateTo({
+      url: '/pages/history/history',
+      success:function(res){
+      },
+      fail:function(error){
+      }
+    })
+  },
+  toMy:function toMy(){
+    wx.switchTab({
+      url: '/pages/my/my',
+    })
+  },
+  toWrong:function toWrong(){
+    wx.navigateTo({
+      url: '/pages/wrong/wrong',
+      success:function(res){
+      },
+      fail:function(error){
+      }
+    })
+  },
+  
+
+
   /**
    * 生命周期函数--监听页面加载
    */
